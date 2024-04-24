e681c6de7634689f7e1eb86d07cc0961adbf4336
toiaa <victoria@geddes.com.ar>
2024-04-24 14:47:15 -0300

    post schema, onboarding schema

--- zodSchemas/authSchemas.ts
+++ zodSchemas/authSchemas.ts
@@ -26,3 +26,16 @@ export const userLoginSchema = z.object({
       .min(8, { message: "must be at least 8 characters long." }),
   }),
 });
+
+export const onBoardingSchema = z.object({
+  body: z.object({
+    journey: z.string().min(4, {
+      message: "must be at least 4 characters",
+    }),
+    ambitions: z
+      .string()
+      .array()
+      .nonempty({ message: "must choose at least one" }),
+    tech: z.string().array().nonempty({ message: "must choose at least one" }),
+  }),
+});
