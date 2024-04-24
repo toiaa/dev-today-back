4ccc4b62456d4761538f1fcc70d35bebd57f4a1e
toiaa <victoria@geddes.com.ar>
2024-04-23 21:04:02 -0300

    ZOD: added schemas, valitade function, types and middlewares

--- routes/auth.js
+++ routes/auth.js
@@ -2,13 +2,18 @@ const express = require("express");
 const bcrypt = require("bcrypt");
 const { prisma } = require("../db");
 const router = express.Router();
-
+const { validate } = require("../middlewares/authMiddleware.ts");
 const saltRounds = 10;
 const saltRoundsRandom = bcrypt.genSaltSync(saltRounds);
+const {
+  userRegisterchema,
+  userLoginSchema,
+} = require("../zodSchemas/authSchemas");
 
-router.post("/register", async (req, res) => {
+router.post("/register", validate(userRegisterchema), async (req, res) => {
   const requestBody = req.body;
   if (!requestBody) return res.status(400).send("No body");
+
   try {
     const hashedPassword = bcrypt.hashSync(
       requestBody.password,
@@ -37,7 +42,7 @@ router.post("/register", async (req, res) => {
   }
 });
 
-router.post("/login", async (req, res) => {
+router.post("/login", validate(userLoginSchema), async (req, res) => {
   const requestBody = req.body;
   console.log("emailrequest", requestBody.email);
   if (!requestBody) return res.status(400).json({ message: "No request body" });
--- zodSchemas/authSchemas.ts
+++ zodSchemas/authSchemas.ts
@@ -1,6 +1,24 @@
 import { z } from "zod";
 export const userRegisterchema = z.object({
-  username: z.string().min(4).max(20),
-  email: z.string().email(),
-  password: z.string().min(8).max(10),
+  body: z.object({
+    username: z.string({ required_error: " is required" }).min(4).max(20),
+    email: z
+      .string({ required_error: "is required" })
+      .email("Email must be valid"),
+    password: z.string().min(8).max(10),
+  }),
+});
+
+export const userLoginSchema = z.object({
+  body: z.object({
+    email: z
+      .string()
+      .min(4, {
+        message: "must be at least 4 characters",
+      })
+      .email("must be valid"),
+    password: z
+      .string({ required_error: "is required" })
+      .min(4, { message: "must be at least 4 characters" }),
+  }),
 });
