b6b5d060fcc4458182f0214197ffc47093bc53c8
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-04-29 22:17:59 +0100

    add idParameterSchema

--- routes/profile.ts
+++ routes/profile.ts
@@ -3,7 +3,7 @@ import { Router, Request, Response } from "express";
 import { prisma } from "../db";
 import { StatusCodes } from "http-status-codes";
 import { validate } from "../middlewares/authMiddleware";
-import { onBoardingSchema } from "../zodSchemas/authSchemas";
+import { idParameterSchema, onBoardingSchema } from "../zodSchemas/authSchemas";
 
 const router = Router();
 
@@ -37,50 +37,56 @@ router.post(
   },
 );
 
-router.get("/onboarding/:id", async (req: Request, res: Response) => {
-  const id = req.params.id;
-  if (!id) return res.status(StatusCodes.BAD_REQUEST).send("No user id");
-  try {
-    const user = await prisma.profile.findUnique({
-      where: {
-        userId: id,
-      },
-    });
-    return res.status(StatusCodes.OK).json(user);
-  } catch (error) {
-    console.error(error);
-    res
-      .status(StatusCodes.INTERNAL_SERVER_ERROR)
-      .json({ message: "Internal server error" });
-  }
-});
+router.get(
+  "/onboarding/:id",
+  validate(idParameterSchema),
+  async (req: Request, res: Response) => {
+    const id = req.params.id;
+    try {
+      const user = await prisma.profile.findUnique({
+        where: {
+          userId: id,
+        },
+      });
+      return res.status(StatusCodes.OK).json(user);
+    } catch (error) {
+      console.error(error);
+      res
+        .status(StatusCodes.INTERNAL_SERVER_ERROR)
+        .json({ message: "Internal server error" });
+    }
+  },
+);
 
-router.put("/onboarding/:id", async (req: Request, res: Response) => {
-  const id = req.params.id;
-  if (!id) return res.status(StatusCodes.BAD_REQUEST).send("No user id");
-  const requestBody = req.body;
-  try {
-    const { journey, ambitions, tech } = requestBody;
-    if (!journey || !ambitions || !tech)
-      return res.status(StatusCodes.BAD_REQUEST).send("Missing fields");
-    const modifyUserOnboarding = await prisma.profile.update({
-      where: {
-        userId: id,
-      },
-      data: {
-        journey,
-        ambitions,
-        tech,
-      },
-    });
+router.put(
+  "/onboarding/:id",
+  validate(idParameterSchema),
+  async (req: Request, res: Response) => {
+    const id = req.params.id;
+    const requestBody = req.body;
+    try {
+      const { journey, ambitions, tech } = requestBody;
+      if (!journey || !ambitions || !tech)
+        return res.status(StatusCodes.BAD_REQUEST).send("Missing fields");
+      const modifyUserOnboarding = await prisma.profile.update({
+        where: {
+          userId: id,
+        },
+        data: {
+          journey,
+          ambitions,
+          tech,
+        },
+      });
 
-    return res.status(StatusCodes.OK).json(modifyUserOnboarding);
-  } catch (error) {
-    console.error(error);
-    res
-      .status(StatusCodes.INTERNAL_SERVER_ERROR)
-      .json({ message: "Internal server error" });
-  }
-});
+      return res.status(StatusCodes.OK).json(modifyUserOnboarding);
+    } catch (error) {
+      console.error(error);
+      res
+        .status(StatusCodes.INTERNAL_SERVER_ERROR)
+        .json({ message: "Internal server error" });
+    }
+  },
+);
 
 export = router;
