b9b5381d05d1b7c84b35e0c05e1d1bd420647f97
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-04-29 22:21:03 +0100

    Add idParameterSchema and validate middleware

--- routes/posts.ts
+++ routes/posts.ts
@@ -1,33 +1,37 @@
 import { Router, Request, Response } from "express";
 import { prisma } from "../db";
 import { StatusCodes } from "http-status-codes";
+import { idParameterSchema } from "../zodSchemas/authSchemas";
+import { validate } from "../middlewares/authMiddleware";
 const router = Router();
 
-router.get("/:id", async (req: Request, res: Response) => {
-  const id = req.params.id;
-  if (!id)
-    return res.status(StatusCodes.BAD_REQUEST).send("No post id provided");
-  try {
-    const posts = await prisma.post.findMany({
-      where: {
-        id,
-      },
-      include: {
-        tags: true,
-      },
-    });
-    if (posts.length === 0) {
-      return res.status(404).send("No posts found");
-    } else {
-      return res.status(StatusCodes.OK).json(posts);
+router.get(
+  "/:id",
+  validate(idParameterSchema),
+  async (req: Request, res: Response) => {
+    const id = req.params.id;
+    try {
+      const posts = await prisma.post.findMany({
+        where: {
+          id,
+        },
+        include: {
+          tags: true,
+        },
+      });
+      if (posts.length === 0) {
+        return res.status(StatusCodes.NOT_FOUND).send("No posts found");
+      } else {
+        return res.status(StatusCodes.OK).json(posts);
+      }
+    } catch (error) {
+      console.error(error);
+      return res
+        .status(StatusCodes.INTERNAL_SERVER_ERROR)
+        .json({ message: "Internal server error" });
     }
-  } catch (error) {
-    console.error(error);
-    return res
-      .status(StatusCodes.INTERNAL_SERVER_ERROR)
-      .json({ message: "Internal server error" });
-  }
-});
+  },
+);
 
 // router.post("/", async (req: Request, res: Response) => {
 //   const requestBody = req.body;
@@ -78,50 +82,55 @@ router.get("/:id", async (req: Request, res: Response) => {
 //   }
 // });
 
-router.delete("/:id", async (req: Request, res: Response) => {
-  const id = req.params.id;
-  if (!id)
-    return res.status(StatusCodes.BAD_REQUEST).send("No post id provided ");
-  try {
-    const postFound = await prisma.post.delete({
-      where: {
-        id,
-      },
-    });
+router.delete(
+  "/:id",
+  validate(idParameterSchema),
+  async (req: Request, res: Response) => {
+    const id = req.params.id;
+    try {
+      const postFound = await prisma.post.delete({
+        where: {
+          id,
+        },
+      });
 
-    return res.status(StatusCodes.OK).json(postFound);
-  } catch (error) {
-    console.error(error);
-    return res
-      .status(StatusCodes.INTERNAL_SERVER_ERROR)
-      .json({ message: "Internal server error" });
-  }
-});
-
-router.get("api/user/:userId/post/", async (req: Request, res: Response) => {
-  const userId = req.params.id;
-  if (!userId) return res.status(StatusCodes.BAD_REQUEST).send("No user id");
-  try {
-    const postsFound = await prisma.post.findMany({
-      where: {
-        authorId: userId,
-      },
-      include: {
-        tags: true,
-      },
-    });
-
-    if (postsFound.length === 0) {
-      return res.status(StatusCodes.NOT_FOUND).json(postsFound);
-    } else {
-      return res.status(StatusCodes.OK).json(postsFound);
+      return res.status(StatusCodes.OK).json(postFound);
+    } catch (error) {
+      console.error(error);
+      return res
+        .status(StatusCodes.INTERNAL_SERVER_ERROR)
+        .json({ message: "Internal server error" });
     }
-  } catch (error) {
-    console.error(error);
-    return res
-      .status(StatusCodes.INTERNAL_SERVER_ERROR)
-      .json({ message: "Internal server error" });
-  }
-});
+  },
+);
+
+router.get(
+  "api/user/:userId/post/",
+  validate(idParameterSchema),
+  async (req: Request, res: Response) => {
+    const userId = req.params.id;
+    try {
+      const postsFound = await prisma.post.findMany({
+        where: {
+          authorId: userId,
+        },
+        include: {
+          tags: true,
+        },
+      });
+
+      if (postsFound.length === 0) {
+        return res.status(StatusCodes.NOT_FOUND).json(postsFound);
+      } else {
+        return res.status(StatusCodes.OK).json(postsFound);
+      }
+    } catch (error) {
+      console.error(error);
+      return res
+        .status(StatusCodes.INTERNAL_SERVER_ERROR)
+        .json({ message: "Internal server error" });
+    }
+  },
+);
 
 export = router;
