c1c7c2e86dd33ad49d687443cc45384ba4e3c5a7
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-05-06 11:52:37 +0100

    Reorder schemas, edit postSchema, add updatePostSchema

--- zodSchemas/postSchemas.ts
+++ zodSchemas/postSchemas.ts
@@ -1,19 +1,4 @@
 import { z } from "zod";
-export const postSchema = z.object({
-  body: z
-    .object({
-      title: z
-        .string()
-        .min(4, { message: "must be at least 4 characters long." }),
-      type: z.string().min(4, { message: "must choose one." }),
-
-      content: z
-        .string()
-        .min(4, { message: "must be at least 4 characters long." }),
-      tags: z.string().array().nonempty({ message: "must add at least one" }),
-    })
-    .required(),
-});
 
 export const idParameterSchema = z.object({
   params: z
@@ -21,6 +6,41 @@ export const idParameterSchema = z.object({
       id: z.string({ required_error: "is required" }).length(36, {
         message: "Not a valid ID",
       }),
+    })
+    .required(),
+});
+
+export const postSchema = z.object({
+  body: z
+    .object({
+      title: z
+        .string()
+        .min(4, { message: "must be at least 4 characters long." }),
+      // type: z.string().min(4, { message: "must choose one." }),
+
+      content: z
+        .string()
+        .min(4, { message: "must be at least 4 characters long." }),
+      // tags: z.string().array().nonempty({ message: "must add at least one" }),
+      authorId: z.string({ required_error: "is required" }).length(36, {
+        message: "Not a valid ID",
+      }),
+    })
+    .required(),
+});
+
+export const updatePostSchema = z.object({
+  body: z
+    .object({
+      title: z
+        .string()
+        .min(4, { message: "must be at least 4 characters long." }),
+      // type: z.string().min(4, { message: "must choose one." }),
+
+      content: z
+        .string()
+        .min(4, { message: "must be at least 4 characters long." }),
+      // tags: z.string().array().nonempty({ message: "must add at least one" }),
     })
     .required(),
 });
