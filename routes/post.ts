85746241dc9b4bb37590281d9f281529d35fed7b
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-05-06 11:50:53 +0100

    Add validation and remove unnecessary route.

--- routes/post.ts
+++ routes/post.ts
@@ -1,7 +1,11 @@
 import { Router, Request, Response } from "express";
 import { prisma } from "../lib/prisma";
 import { StatusCodes } from "http-status-codes";
-import { idParameterSchema } from "../zodSchemas/authSchemas";
+import {
+  idParameterSchema,
+  postSchema,
+  updatePostSchema,
+} from "../zodSchemas/postSchemas";
 import { validate } from "../middlewares/authMiddleware";
 const router = Router();
 
@@ -35,9 +39,8 @@ router.get(
 );
 
 //create a post
-router.post("/", async (req: Request, res: Response) => {
+router.post("/", validate(postSchema), async (req: Request, res: Response) => {
   const { title, content, authorId } = req.body;
-  if (!authorId) return res.status(StatusCodes.BAD_REQUEST).send("No user id");
   try {
     const post = await prisma.post.create({
       data: {
@@ -56,31 +59,33 @@ router.post("/", async (req: Request, res: Response) => {
   }
 });
 
-// router.patch("/:id", async (req: Request, res: Response) => {
-//   const id = req.params.id;
-//   if (!id)
-//     return res.status(StatusCodes.BAD_REQUEST).send("No post id provided");
-//   const requestBody = req.body;
-//   const { title, content, tags } = requestBody;
-//   try {
-//     const posts = await prisma.post.update({
-//       where: {
-//         id,
-//       },
-//       data: {
-//         title,
-//         content,
-//         tags,
-//       },
-//     });
-//     if (posts.length === 0) return res.status(StatusCodes.OK).json(posts);
-//   } catch (error) {
-//     console.error(error);
-//     return res
-//       .status(StatusCodes.INTERNAL_SERVER_ERROR)
-//       .json({ message: "Internal server error" });
-//   }
-// });
+//edit a post with its id
+router.patch(
+  "/:id",
+  validate(idParameterSchema),
+  validate(updatePostSchema),
+  async (req: Request, res: Response) => {
+    const id = req.params.id;
+    const { title, content } = req.body;
+    try {
+      const posts = await prisma.post.update({
+        where: {
+          id,
+        },
+        data: {
+          title,
+          content,
+        },
+      });
+      return res.status(StatusCodes.OK).json(posts);
+    } catch (error) {
+      console.error(error);
+      return res
+        .status(StatusCodes.INTERNAL_SERVER_ERROR)
+        .json({ message: "Internal server error" });
+    }
+  },
+);
 
 //delete a single post
 router.delete(
@@ -104,34 +109,5 @@ router.delete(
     }
   },
 );
-
-// router.get(
-//   "api/user/:userId/post/",
-//   validate(idParameterSchema),
-//   async (req: Request, res: Response) => {
-//     const userId = req.params.id;
-//     try {
-//       const postsFound = await prisma.post.findMany({
-//         where: {
-//           authorId: userId,
-//         },
-//         include: {
-//           tags: true,
-//         },
-//       });
-
-//       if (postsFound.length === 0) {
-//         return res.status(StatusCodes.NOT_FOUND).json(postsFound);
-//       } else {
-//         return res.status(StatusCodes.OK).json(postsFound);
-//       }
-//     } catch (error) {
-//       console.error(error);
-//       return res
-//         .status(StatusCodes.INTERNAL_SERVER_ERROR)
-//         .json({ message: "Internal server error" });
-//     }
-//   },
-// );
 
 export default router;
