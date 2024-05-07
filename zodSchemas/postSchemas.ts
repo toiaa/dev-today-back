7e82213a1cc1224b438a676eb0ef9c4ef0a5b98b
mjlearn13 <80919195+mjlearn13@users.noreply.github.com>
2024-05-07 12:46:55 +0100

    Create general authSchema and apply to userRegisterSchema and userLoginSchema using omit where necessary.

--- zodSchemas/postSchemas.ts
+++ zodSchemas/postSchemas.ts
@@ -10,37 +10,23 @@ export const idParameterSchema = z.object({
     .required(),
 });
 
-export const postSchema = z.object({
-  body: z
-    .object({
-      title: z
-        .string()
-        .min(4, { message: "must be at least 4 characters long." }),
-      // type: z.string().min(4, { message: "must choose one." }),
+//general schema that has all fields, where necessary we take the required ones that we need for a specific validation.
+export const generalPostSchema = z.object({
+  title: z.string().min(4, { message: "must be at least 4 characters long." }),
+  // type: z.string().min(4, { message: "must choose one." }),
+  content: z
+    .string()
+    .min(4, { message: "must be at least 4 characters long." }),
+  // tags: z.string().array().nonempty({ message: "must add at least one" }),
+  authorId: z.string({ required_error: "is required" }).length(36, {
+    message: "Not a valid ID",
+  }),
+});
 
-      content: z
-        .string()
-        .min(4, { message: "must be at least 4 characters long." }),
-      // tags: z.string().array().nonempty({ message: "must add at least one" }),
-      authorId: z.string({ required_error: "is required" }).length(36, {
-        message: "Not a valid ID",
-      }),
-    })
-    .required(),
+export const postSchema = z.object({
+  body: generalPostSchema.required(),
 });
 
 export const updatePostSchema = z.object({
-  body: z
-    .object({
-      title: z
-        .string()
-        .min(4, { message: "must be at least 4 characters long." }),
-      // type: z.string().min(4, { message: "must choose one." }),
-
-      content: z
-        .string()
-        .min(4, { message: "must be at least 4 characters long." }),
-      // tags: z.string().array().nonempty({ message: "must add at least one" }),
-    })
-    .required(),
+  body: generalPostSchema.pick({ title: true, content: true }).required(),
 });
